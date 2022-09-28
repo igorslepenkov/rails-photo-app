ced95c10a08c0aa8f2332c1af069102162e93a21
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-29 00:06:43 +0300

    feat(photo-app): add payment form to app(without correct sign up)

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,35 @@
+require 'stripe'
+require 'json'
+Rails.configuration.stripe = {
+  pulishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  secrete_key: ENV['STRIPE_TEST_SECRETE_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secrete_key]
+
+class PaymentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+
+  # Securely calculate the order amount
+  def calculate_order_amount(_plan)
+    plans_prices = {
+      premium: 10,
+      amaze: 20
+    }
+
+    (plans_prices[_plan.to_sym].to_f * 4.94).round.to_i
+  end
+
+  def create_intent
+    plan_amount = calculate_order_amount(params.require('plan'))
+    payment_intent = Stripe::PaymentIntent.create(
+      amount: plan_amount * 100,
+      currency: 'pln',
+      automatic_payment_methods: {
+        enabled: true
+      }
+    )
+    render json: { clientSecret: payment_intent['client_secret'] }
+  end
+end
--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  pulishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
-  secrete_key: ENV['STRIPE_TEST_SECRETE_KEY']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secrete_key]
